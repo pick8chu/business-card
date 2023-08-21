21d02a5b6c97208154c39ad0e122a4e441072e74
Ethan <po00076@gmail.com>
2023-08-22 00:38:20 +0200

    wip

---
+++ script.js
@@ -0,0 +1,30 @@
+const businessCard = document.querySelector('.business-card');
+let isDragging = false; // Flag to track dragging state
+
+businessCard.addEventListener('mousedown', (event) => {
+  isDragging = false; // Initialize dragging flag
+
+  // Add event listeners for mousemove and mouseup
+  document.addEventListener('mousemove', handleMouseMove);
+});
+
+// Handle mousemove event
+function handleMouseMove(event) {
+  // Calculate the distance moved during dragging
+  const threshold = 5; // Adjust this threshold as needed
+  const deltaX = Math.abs(event.movementX);
+  const deltaY = Math.abs(event.movementY);
+
+  // If the movement surpasses the threshold, consider it a drag
+  if (deltaX > threshold || deltaY > threshold) {
+    isDragging = true;
+  }
+}
+
+businessCard.addEventListener('click', (event) => {
+  if (isDragging) {
+    event.preventDefault(); // Prevent the click event during dragging
+  } else {
+    businessCard.classList.toggle('clicked');
+  }
+});
